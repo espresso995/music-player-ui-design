a71a6a4b4f0cf3d7b5dfb5a574c4373f212a5463
Trung Nguyen <nguyenhongtrung.ptit@gmail.com>
2021-01-22 10:34:44 +0700

    Complete Project

--- app/src/main/java/com/android/trungnh2/musicplayeruidesign/MainActivity.java
+++ app/src/main/java/com/android/trungnh2/musicplayeruidesign/MainActivity.java
@@ -3,12 +3,22 @@ package com.android.trungnh2.musicplayeruidesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.makeramen.roundedimageview.RoundedImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RoundedImageView imageAlbum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageAlbum = findViewById(R.id.imageAlbum);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+        imageAlbum.setAnimation(animation);
     }
 }
